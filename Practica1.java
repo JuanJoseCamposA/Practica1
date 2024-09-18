1ac975f5cab982a435d173bee0c85a4267204d22
Juan Jose Campos <juanjosecampos627@gmail.com>
2024-09-17 19:04:25 -0600

    Nueva Version Area y Perimetro del Circulo

--- Practica1.java
+++ Practica1.java
@@ -1,80 +1,17 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package practica1;
+package practica1.Practica1;
 
 import java.util.Scanner;
 
-/**
- *
- * @author juanj
- */
 public class Practica1 {
-
-    // Método para calcular el área de un círculo
-    public static double areaCirculo(double radio) {
-        return Math.PI * Math.pow(radio, 2);
-    }
-
-    // Método para calcular el área de un rectángulo
-    public static double areaRectangulo(double largo, double ancho) {
-        return largo * ancho;
-    }
-
-    // Método para calcular el área de un triángulo
-    public static double areaTriangulo(double base, double altura) {
-        return (base * altura) / 2;
-    }
-
     public static void main(String[] args) {
+        int radio = 0;
         Scanner scanner = new Scanner(System.in);
-        int opcion;
 
-        do {
-            System.out.println("Calculadora de Áreas");
-            System.out.println("1. Círculo");
-            System.out.println("2. Rectángulo");
-            System.out.println("3. Triángulo");
-            System.out.println("4. Salir");
-            System.out.print("Elige una opción: ");
-            opcion = scanner.nextInt();
-
-            switch (opcion) {
-                case 1:
-                    System.out.print("Introduce el radio del círculo: ");
-                    double radio = scanner.nextDouble();
-                    System.out.println("El área del círculo es: " + areaCirculo(radio));
-                    break;
-                case 2:
-                    System.out.print("Introduce el largo del rectángulo: ");
-                    double largo = scanner.nextDouble();
-                    System.out.print("Introduce el ancho del rectángulo: ");
-                    double ancho = scanner.nextDouble();
-                    System.out.println("El área del rectángulo es: " + areaRectangulo(largo, ancho));
-                    break;
-                case 3:
-                    System.out.print("Introduce la base del triángulo: ");
-                    double base = scanner.nextDouble();
-                    System.out.print("Introduce la altura del triángulo: ");
-                    double altura = scanner.nextDouble();
-                    System.out.println("El área del triángulo es: " + areaTriangulo(base, altura));
-                    break;
-                case 4:
-                    System.out.println("Saliendo...");
-                    break;
-                default:
-                    System.out.println("Opción no válida, intenta nuevamente.");
-            }
-
-            System.out.println();
-
-        } while (opcion != 4);
-
-        scanner.close();
+        System.out.println("Ingresa el tamaño del radio: ");
+        radio = scanner.nextInt();
+            double Area = Math.PI * Math.pow(radio, 2);
+            double perimetro = 2 * Math.PI * radio;
+            System.out.println("El área del círculo es: " + Area);
+            System.out.println("El perímetro del círculo es: " + perimetro);
     }
 }
-
-
-
